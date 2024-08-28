568241aeecf70750d04853b79ed07f23e7079c10
CarolShingai <carolineshingai@gmail.com>
2024-08-28 20:58:50 -0300

    death problems

--- srcs/death.c
+++ srcs/death.c
@@ -6,7 +6,7 @@
 /*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 20:08:27 by cshingai          #+#    #+#             */
-/*   Updated: 2024/08/28 20:50:15 by cshingai         ###   ########.fr       */
+/*   Updated: 2024/08/28 20:56:43 by cshingai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,10 @@ void	is_philo_dead(t_philo *philo)
 
 void	monitoring(t_table *table)
 {
-	if (create_thread(table) == TRUE && table->philo->life_status != DIED)
+	create_thread(table);
+	if (table->philo->life_status != DIED)
 	{
+		printf("entrou");
 		is_philo_dead(table->philo);
 		// if (table->philo->life_status == DIED)
 		// {
